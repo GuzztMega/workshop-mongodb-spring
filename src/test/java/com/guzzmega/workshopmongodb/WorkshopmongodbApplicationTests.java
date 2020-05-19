bb1fbdfda9627431b4d0d956096530c5bb4d81e2
GuzzMega <guzzt.mega@gmail.com>
2020-05-19 15:41:09 -0300

    Projeto criado

---
+++ src/test/java/com/guzzmega/workshopmongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guzzmega.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
