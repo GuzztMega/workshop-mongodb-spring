2ad70ae48ed9bb82afb9c1a170403e705bc4efa7
GuzzMega <guzzt.mega@gmail.com>
2020-05-20 20:57:50 -0300

    Acrescentando Comentários aos Posts

--- src/main/java/com/guzzmega/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/guzzmega/workshopmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.guzzmega.workshopmongodb.config;
 import com.guzzmega.workshopmongodb.domain.Post;
 import com.guzzmega.workshopmongodb.domain.User;
 import com.guzzmega.workshopmongodb.dto.AuthorDTO;
+import com.guzzmega.workshopmongodb.dto.CommentDTO;
 import com.guzzmega.workshopmongodb.repositories.PostRepository;
 import com.guzzmega.workshopmongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,13 +34,19 @@ public class Instantiation implements CommandLineRunner {
 
         User mary = new User( null, "Mary Brown", "mary@hotmail.com");
         User alex = new User(null, "Alex Gray", "alex@yahoo.com");
-        User bob = new User( null, "Bob White", "bob@gmail.com");
+        User ayron = new User( null, "Ayron Silver", "ayron@gmail.com");
 
-        userRepository.saveAll(Arrays.asList(mary, alex, bob));
+        userRepository.saveAll(Arrays.asList(mary, alex, ayron));
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Lets ride!!!", "Heading to the beach...", new AuthorDTO(mary));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Moooorning!", "What a beautiful day", new AuthorDTO(mary));
 
+        CommentDTO c1 = new CommentDTO("Nice trip, bro!", sdf.parse("21/03/2018"), new AuthorDTO(ayron));
+        CommentDTO c2 = new CommentDTO("Enjoooy", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c3 = new CommentDTO("Have a nice day!", sdf.parse("23/03/2018"), new AuthorDTO(ayron));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
         postRepository.saveAll(Arrays.asList(post1, post2));
 
         mary.getPosts().addAll(Arrays.asList(post1, post2));
